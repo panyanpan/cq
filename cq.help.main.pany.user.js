81ef629b7646bdbc0ddd18e9f27b489a93fbe0db
pany <panyanpan@qq.com>
2026-09-06 18:00:07 +0800

    p20260906.03

--- cq.help.main.pany.user.js
+++ cq.help.main.pany.user.js
@@ -801,7 +801,7 @@
             }
             para_mochaoCount++;
             try { findMochao_Occupy(); }
-            catch (error) { console.error("time-findMochao_Occupy-error:" + error.message); }            
+            catch (error) { console.error("time-findMochao_Occupy-error:" + error.message); }
         }, 4000);
         p_alert_success('开始（Shentai）');
     }
@@ -1361,8 +1361,8 @@
     addUIControl(sel1Dom, { right: 120, top: 35 });
     addUIControl(mapSelDom, { right: 100, top: 10, width: 150 });
 
-    // addUIControl(f_CreateButton("RongL", beginTimer_f_Ronglian), { right: 30, top: 130 });
-    // addUIControl(f_CreateButton("ShenT", beginTimer_f_Ronglian), { right: 70, top: 130 });
+    // addUIControl(f_CreateButton("熔炼", beginTimer_f_Ronglian), { right: 30, top: 130 });
+    // addUIControl(f_CreateButton("神台", findMochao_Occupy), { right: 70, top: 130 });
 
     loadYijiConfig();
     loadMapConfig();
